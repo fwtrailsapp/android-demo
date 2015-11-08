c2f0b7bd3de9cb19f151f75f0dab74d280842c9f
Jaron <somejj01@students.ipfw.edu>
2015-11-08 16:06:33 -0500

    Updated coordinates to skybridge

--- app/src/main/java/com/example/jaron/fwtrailsapp_android_demo/MapsActivity.java
+++ app/src/main/java/com/example/jaron/fwtrailsapp_android_demo/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng skyBridge = new LatLng(41, -85);
+        mMap.addMarker(new MarkerOptions().position(skyBridge).title("IPFW Skybridge"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(skyBridge));
     }
 }
